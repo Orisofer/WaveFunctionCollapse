e485b6a71de2cba985908e517e8f826de8036427
Ori Sofer <orisofercetem@gmail.com>
2024-11-26 00:03:43 +0200

    - cleaned up code structure - introduced WFC Controller and WFC - control flow and algorithm is decoupled now - added optional callback when algo is finished

--- Assets/Scripts/Runtime/WaveFunctionCollapse.cs
+++ Assets/Scripts/Runtime/WaveFunctionCollapse.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using Cysharp.Threading.Tasks;
@@ -8,66 +9,98 @@ namespace WFC
 {
     public class WaveFunctionCollapse : MonoBehaviour
     {
-        [SerializeField] private Tile[] Tiles;
-        
-        private Dictionary<GridCell, List<Tile>> m_AvailableTilesForCell = new Dictionary<GridCell, List<Tile>>();
-        private List<GridCell> m_Cells = new List<GridCell>();
-        
+        [SerializeField] private GameObject m_GridCellprefab;
+        [SerializeField] private Tile[] m_Tiles;
+
+        private List<GridCell> m_Cells;
         private int m_GridWidth;
         private int m_GridHeight;
         private int m_NumCollapsed;
-
-        public bool Finished { get; private set; }
+        private bool m_GridReady;
+        private bool m_Finished;
         
         //TODO: 2) lakes doesn't generate properly, lots of misses. good guess is to check from what source the algorithm-
         //TODO: -moves to the next cell to collapse (currently its from all the cells, maybe should be a queue or stack
-        
-        public void SetGridDimentions(int width, int height)
+
+        // API Call for grid initialization
+        public void GenerateGrid(int width, int height, out Vector3 gridCenterPoint)
         {
-            Finished = false;
+            SetGridDimentions(width, height);
+
+            if (!m_GridReady)
+            {
+                Debug.Log("WFC: Trying to generate a grid with missing/invalid parameters");
+                gridCenterPoint = Vector3.zero;
+                return;
+            }
+            
+            Vector3 alignCenter = new Vector3(((float)m_GridWidth / 2f) - 0.5f, ((float)m_GridHeight / 2f) - 0.5f, -10f);
+            gridCenterPoint = alignCenter;
+            
+            // create grid holder game object
+            GameObject gridHolder = new GameObject("GridHolder");
+            gridHolder.transform.SetParent(transform);
+                
+            for (int x = 0; x < m_GridWidth; x++)
+            {
+                for (int y = 0; y < m_GridHeight; y++)
+                {
+                    GameObject newCellGameObject = Instantiate(m_GridCellprefab, gridHolder.transform);
+                    
+                    Vector3 position = new Vector3(x, y, 0);
+                    newCellGameObject.transform.position = position;
+                    newCellGameObject.name = $"GridCell:({x},{y})";
+                    
+                    InitCell(newCellGameObject, new Vector2Int((int)position.x, (int)position.y));
+                }
+            }
+        }
+        
+        private void SetGridDimentions(int width, int height)
+        {
+            m_Finished = false;
             
             m_GridWidth = width;
             m_GridHeight = height;
             m_NumCollapsed = 0;
+            
+            m_Cells = new List<GridCell>(m_GridWidth * m_GridHeight);
+
+            m_GridReady = true;
         }
         
-        public async UniTask<bool> GenerateAuto()
+        
+        // API Call for auto generate
+        public async UniTask GenerateAuto(int delay = 0, Action finishedCallback = null)
         {
-            // // pick cell to collapse
-            // GridCell currentCell = GetRandomCellFromList(m_Cells);
-            // NextCollapse(currentCell);
-
             while (m_NumCollapsed < m_Cells.Count)
             {
                 // pick the next cell with the lowest entropy
                 GridCell currentCell = GetLowestEntropyCell();
                 NextCollapse(currentCell);
-                await UniTask.Delay(5);
+                await UniTask.Delay(delay);
             }
 
-            if (m_NumCollapsed == m_Cells.Count)
+            if (CheckFinished())
             {
-                Debug.Log("WFC: Success!");
-                Finished = true;
-                return true;
+                finishedCallback?.Invoke();
             }
-            
-            Finished = true;
-            return false;
         }
 
-        public bool GenerateStep()
+        // API Call for generate step by step
+        public void GenerateStep(int numSteps = 1, Action finishedCallback = null)
         {
-            GridCell currentCell = GetLowestEntropyCell();
-            NextCollapse(currentCell);
-            
-            if (m_NumCollapsed == m_Cells.Count)
+            for (int i = 0; i < numSteps; i++)
             {
-                Debug.Log("WFC: Success!");
-                return true;
+                if (CheckFinished())
+                {
+                    finishedCallback?.Invoke();
+                    break;
+                }
+                
+                GridCell currentCell = GetLowestEntropyCell();
+                NextCollapse(currentCell);
             }
-            
-            return false;
         }
 
         private void NextCollapse(GridCell currentCell)
@@ -87,12 +120,12 @@ namespace WFC
             }
         }
         
-        public void InitCell(GameObject cellGameObject, Vector2Int position)
+        private void InitCell(GameObject cellGameObject, Vector2Int position)
         {
             GridCell newGridCell = cellGameObject.GetComponent<GridCell>();
             m_Cells.Add(newGridCell);
             
-            newGridCell.InitCell(position, Tiles);
+            newGridCell.InitCell(position, m_Tiles);
         }
 
         private GridCell GetLowestEntropyCell()
@@ -156,14 +189,14 @@ namespace WFC
             return newAvailableTiles;
         }
 
-        private Dictionary<CellDirection, GridCell> GetCellNeighbors(GridCell firstCell)
+        private Dictionary<CellDirection, GridCell> GetCellNeighbors(GridCell cell)
         {
             Dictionary<CellDirection, GridCell> neighbors = new Dictionary<CellDirection, GridCell>()
             {
-                {CellDirection.Up, GetCellInDirection(firstCell, CellDirection.Up)},
-                {CellDirection.Down, GetCellInDirection(firstCell, CellDirection.Down)},
-                {CellDirection.Left, GetCellInDirection(firstCell, CellDirection.Left)},
-                {CellDirection.Right, GetCellInDirection(firstCell, CellDirection.Right)},
+                {CellDirection.Up, GetCellInDirection(cell, CellDirection.Up)},
+                {CellDirection.Down, GetCellInDirection(cell, CellDirection.Down)},
+                {CellDirection.Left, GetCellInDirection(cell, CellDirection.Left)},
+                {CellDirection.Right, GetCellInDirection(cell, CellDirection.Right)},
             };
 
             return neighbors;
@@ -195,6 +228,19 @@ namespace WFC
             }
             
             return null;
+        }
+
+        private bool CheckFinished()
+        {
+            if (m_NumCollapsed == m_Cells.Count)
+            {
+                Debug.Log("WFC: Success!");
+                m_Finished = true;
+                return true;
+            }
+            
+            Debug.Log("WFC: Failed!");
+            return false;
         }
     }
 }
